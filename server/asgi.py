9e78d302dd70fba267ef8886fc2ec4dbeca0a0dc
Anton <it2015maistrenko@gmail.com>
2018-11-04 19:56:53 +0200

    Fix get_channel_layer

--- server/asgi.py
+++ server/asgi.py
@@ -1,5 +1,5 @@
 import os
-import channels.asgi
+from channels.layers import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-channel_layer = channels.asgi.get_channel_layer()
+channel_layer = get_channel_layer()
